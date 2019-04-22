e5bab16506752fc8a5664c897e6594c9ea03b78f
Tyler Turnipseed <tyler@techturnip.us>
2019-04-21 22:18:58 -0400

    started JavaScript III project

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Implicit binding, is utilizing the 'this' keyword in an implied manner.
+ * 2. Explicit binding, is when you define the context you want 'this' to refer to.
+ * 3. New binding,
+ * 4.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
